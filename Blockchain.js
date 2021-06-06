1947c23a96ad8bbd4058e2c3eff5e7cfba8d9ad7
imraymondlee <imleeraymond@gmail.com>
2021-06-06 14:12:36 -0400

    Implement adding blocks to the blockchain

--- Blockchain.js
+++ Blockchain.js
@@ -8,6 +8,17 @@ class Blockchain {
   createGenesisBlock() {
     return new Block(0, '06/06/2021', 'Genesis Block', '0');
   }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    //hash needs to be updated as previous hash has been updated
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
 }
 
 module.exports = Blockchain;
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const Block = require('./Block');
 const Blockchain = require('./Blockchain');
 
-const b = new Blockchain();
-console.log('B:', b);
+const irlCoin = new Blockchain();
+
+irlCoin.addBlock(
+  new Block(1, '06/06/2021', { sender: 'Ray', recipient: 'John', amount: 10 })
+);
+
+irlCoin.addBlock(
+  new Block(2, '06/06/2021', { sender: 'Ray', recipient: 'Nancy', amount: 50 })
+);
+
+console.log('irlCoin:', JSON.stringify(irlCoin, null, 4));
