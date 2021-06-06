8684d5e6c1998e270a61b9dd566fabdbd98bb359
imraymondlee <imleeraymond@gmail.com>
2021-06-06 13:31:18 -0400

    Create Block class

---
+++ Block.js
@@ -0,0 +1,21 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  //calculates current block's hash based on its properties
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.previousHash
+    ).toString();
+  }
+}
